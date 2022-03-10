4eec861be832da6242fba95ffaee670688572dee
snamiki1212 <snamiki1212@gmail.com>
2022-03-09 17:38:48 -0800

    feat: add box example

---
+++ src/exampleBox.ts
@@ -0,0 +1,50 @@
+import { generate } from "./generator";
+import type { DocDefinition, Options } from "./generator";
+
+const docDefinition: DocDefinition = {
+  content: [
+    "This paragraph fills full width, as there are no columns. Next paragraph however consists of three columns",
+    {
+      style: "box",
+      table: {
+        widths: ["*"],
+        body: [
+          [
+            {
+              text: "This is first section.\nThis is second line.\nTHis is third line.",
+            },
+          ],
+        ],
+      },
+      layout: {
+        paddingBottom() {
+          return 16;
+        },
+        paddingLeft() {
+          return 16;
+        },
+        paddingRight() {
+          return 16;
+        },
+        paddingTop() {
+          return 16;
+        },
+        hLineWidth() {
+          return 0;
+        },
+        vLineWidth() {
+          return 0;
+        },
+      },
+    },
+  ],
+  styles: {
+    box: {
+      fillColor: "#F7F7F7", // TODO: use constant
+    },
+  },
+};
+
+const options: Options = {};
+
+generate("output/exampleBox.pdf", docDefinition, options);
