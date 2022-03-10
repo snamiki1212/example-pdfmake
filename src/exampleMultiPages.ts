930533bd8589b57b1d5d341d129169ad67197bd0
snamiki1212 <snamiki1212@gmail.com>
2022-03-09 17:59:27 -0800

    feat: update multi pages

--- src/exampleMultiPages.ts
+++ src/exampleMultiPages.ts
@@ -1,3 +1,25 @@
-import { printer } from "./generator";
+import { generate } from "./generator";
+import type { DocDefinition, Options } from "./generator";
 
-console.log("this is multipage");
+const docDefinition: DocDefinition = {
+  content: [
+    "header",
+    {
+      table: {
+        body: [
+          ["#", "item"],
+          ...Array.from({ length: 100 }).map((_, i) => [i, ":D"]),
+        ],
+      },
+    },
+    "last line",
+
+    // new page
+    { text: "", pageBreak: "after" },
+    "Bye Bye",
+  ],
+};
+
+const options: Options = {};
+
+generate("output/exampleMultiPages.pdf", docDefinition, options);
